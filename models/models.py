974fe2419ac5547c06807381ef3628253b7cf80d
Crashroma2009 <Crashroma2009@yandex.ru>
2022-08-29 13:00:46 +0300

    Update models

--- models/models.py
+++ models/models.py
@@ -1,18 +1,20 @@
 from sqlalchemy.orm import relationship
 
-from databases import engine
+#from databases import engine
 
-from sqlalchemy import MetaData, Table, Column, String, Integer, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy import MetaData, create_engine, Table, Column, String, Integer, ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+metadata = MetaData()
+engine = create_engine('sqlite:///sqlite3.db')
 
 
 class Author(Base):
     __tablename__ = 'author'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
-    book = relationship('Book')
+    books = relationship('Book')
 
 
 class Book(Base):
@@ -20,7 +22,11 @@ class Book(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     genre = Column(String(50), nullable=True)
-    author_id = Column(Integer, ForeignKey('author'))
+    author_id = Column(Integer, ForeignKey('author.id'))
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
 
 
 
