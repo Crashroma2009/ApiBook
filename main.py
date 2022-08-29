57a14d764c5301637fbb1e280a595d6ecd785210
Crashroma2009 <Crashroma2009@yandex.ru>
2022-08-29 12:19:05 +0300

    refresh ApiBook

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 def index():
-    return {'Hello, mr': 'Roman'}
-
-
-@app.get("/book/{book_id}")
-async def info(book_id: int):
-    return {'Hello, it book unique number': book_id}
-
-
-
-
+    return f'Hello world'
