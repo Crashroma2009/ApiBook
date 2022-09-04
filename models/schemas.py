8721cad1b4bc4ed73783c973358853ef2993b8ce
Crashroma2009 <Crashroma2009@yandex.ru>
2022-09-04 11:51:01 +0300

    Added schemas.py

--- models/schemas.py
+++ models/schemas.py
@@ -1,14 +1,35 @@
 from pydantic import BaseModel
 
+from models.models import Book
+
 
 class AuthorSchema(BaseModel):
-    id: int
     name: str
+
+
+class AuthorCreate(AuthorSchema):
+    pass
+
+
+class Author(AuthorSchema):
+    id: int
+    books: list[Book] = []
+
+    class Config:
+        orm_mode = True
 
 
 class BookSchema(BaseModel):
     title: str
     genre: str
+
+
+class BookCrate(BookSchema):
+    pass
+
+
+class Book(BookSchema):
+    id: int
     author_id: int
 
     class Config:
