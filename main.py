01805dbdba33eddaebe6c7f4e541e041fc1b645e
Crashroma2009 <Crashroma2009@yandex.ru>
2022-09-05 13:35:57 +0300

    Create main/add_author

--- main.py
+++ main.py
@@ -28,6 +28,15 @@ def read_author(author_id: int, db: Session = Depends(get_db)):
     return author
 
 
+@app.post('/author/', response_model=schemas.Author)
+def create_author(author: schemas.Author, db: Session = Depends(get_db)):
+    add_author = models.Author(name=author.name)
+    db.add(add_author)
+    db.commit()
+    db.refresh(add_author)
+    return add_author
+
+
 @app.get('/books/', response_model=list[schemas.Book])
 def read_books_all(db: Session = Depends(get_db)):
     books = db.query(Book).all()
@@ -44,9 +53,8 @@ def read_book(book_id: int, db: Session = Depends(get_db)):
     return book
 
 
-@app.post('/book_create/', response_model=schemas.Book)
+@app.post('/book/', response_model=schemas.Book)
 def create_book(book: schemas.Book, db: Session = Depends(get_db)):
-    #print(book)
     add_book = models.Book(title=book.title, genre=book.genre, author_id=book.author_id)
     db.add(add_book)
     db.commit()
